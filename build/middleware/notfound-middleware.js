439e356e674eb1e9295c84bf52af5e4c2645ac01
Great Edoseghe <129328859+Gmedulla101@users.noreply.github.com>
2025-05-04 04:13:31 -0700

    Delete build directory

--- build/middleware/notfound-middleware.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const notFound = (req, res) => {
-    res.status(404).json({ sucess: false, msg: 'This route does not exist' });
-};
-exports.default = notFound;
-//# sourceMappingURL=notfound-middleware.js.map
